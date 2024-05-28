30593dd9fb39d77729496f9669d6ec8d2a7d5751
Caffwine <135164957+caffwydev@users.noreply.github.com>
2024-05-28 16:20:01 -0300

    Create TamperMonkeyLD.js

---
+++ TamperMonkeyLD.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        Livro Digital (Professor)
+// @namespace   Hiro Scripts
+// @match       https://livrodigital.sae.digital/@mount*
+// @grant       none
+// @version     1.1
+// @author      Hiro
+// @require     https://github.com/caffwydev/cdn/blob/main/LivroDigital.js?raw=true
+// @description 28/05/2024, 16:01:28
+// ==/UserScript==
